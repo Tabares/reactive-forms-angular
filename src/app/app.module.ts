f82ffbafd2f709e51eab6246940432b55d8cad34
Jose Sotelo <Jose.Sotelo@pressganey.com>
2019-06-06 17:18:52 -0500

    Initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ResgisterComponentComponent } from './resgister-component/resgister-component.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    ResgisterComponentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ResgisterComponentComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/resgister-component/resgister-component.component.ts
+++ src/app/resgister-component/resgister-component.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-resgister-component',
   templateUrl: './resgister-component.component.html',
-  styleUrls: ['./resgister-component.component.css']
+  styleUrls: ['./resgister-component.component.css'],
 })
 export class ResgisterComponentComponent implements OnInit {
+  user = new User('', '', '', '', '');
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  save(resgister: NgForm) {
+    console.log(resgister.form);
+    console.log('Saved: ' + JSON.stringify(resgister.value));
   }
-
 }
