89383c61a11fcff0e7e6e9c3f4ee28de7db315ae
Jose Sotelo <Jose.Sotelo@pressganey.com>
2019-06-06 23:32:17 -0500

    setup reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ResgisterComponentComponent } from './resgister-component/resgister-component.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ResgisterComponentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ResgisterComponentComponent],
+  imports: [ReactiveFormsModule, BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/resgister-component/resgister-component.component.ts
+++ src/app/resgister-component/resgister-component.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-resgister-component',
   templateUrl: './resgister-component.component.html',
-  styleUrls: ['./resgister-component.component.css']
+  styleUrls: ['./resgister-component.component.css'],
 })
 export class ResgisterComponentComponent implements OnInit {
+  name = new FormControl('');
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  updateName() {
+    console.log(this.name);
+
+    this.name.setValue('Jose Emmanuel');
   }
-
 }
